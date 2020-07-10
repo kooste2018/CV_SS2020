d5cb2c495cabce583e3fb27147bcdab211175d57
kooste2018 <40902228+kooste2018@users.noreply.github.com>
2020-07-10 14:28:00 +0800

    Update ImageReader.m
    comments added

--- ImageReader.m
+++ ImageReader.m
@@ -10,64 +10,64 @@ classdef ImageReader
     end
     
     methods
-        function ir = ImageReader(src, L, R, varargin)     
-            if (L==R)
+        function ir = ImageReader(src, L, R, varargin)%function with same name, to get parameters and update properties     
+            if (L==R)%check if the same folder
                 error('L and R must be different!\n');
             else
             % Input Parser
             p=inputParser;
-            p.addRequired('src');
+            p.addRequired('src');%these 3 are required parameter 
             p.addRequired('L',@(x) isnumeric(x) && (x==1) || (x==2));
             p.addRequired('R',@(x) isnumeric(x) && (x==3) || (x==2));
             p.addOptional('start',ir.start,@(x) isnumeric(x));
-            p.addOptional('N',ir.N, @(x) isnumeric(x));
+            p.addOptional('N',ir.N, @(x) isnumeric(x));%these 2 are optional, we have default value for them
             p.parse(src, L, R, varargin{:});
             % update properties of this object ir 
             %check path if something strange happens!
-            %check system
-            if ispc
+            %check system, update full path correctly
+            if ispc%if it's windows
                 ir.l = strcat(src,'\',src(end-5:end),'_C',string(p.Results.L),'\'); 
                 ir.r = strcat(src,'\',src(end-5:end),'_C',string(p.Results.R),'\');
             else
-                if ismac||isunix
+                if ismac||isunix%if it's mac or unix, they have same path syntax 
                     ir.l = strcat(src,'/',src(end-5:end),'_C',string(p.Results.L),'/'); 
                     ir.r = strcat(src,'/',src(end-5:end),'_C',string(p.Results.R),'/');
                 else
-                    error("System not supported!\n");
+                    error("System not supported!\n");%error message
                 end
             end
-            ir.start = p.Results.start;
-            ir.start=max(ir.start,1);%0 means index 1 by me, so set start min 1
-            ir.N = p.Results.N;
-            ir.allimage_left=dir(strcat(ir.l,"*.jpg"));%read all images for next()
+            ir.start = p.Results.start;%index of starting image
+            ir.start=max(ir.start,1);%0 means index 1 for the images, so set start min 1
+            ir.N = p.Results.N;%number of following images
+            ir.allimage_left=dir(strcat(ir.l,"*.jpg"));%read all image informations for next()
             ir.allimage_right=dir(strcat(ir.r,"*.jpg"));
             end
         end
 
-        function [left, right, loop] = next(ir)
-            persistent index;%define and init persistent index, otherwise value lost
+        function [left, right, loop] = next(ir)%function to read image further and further
+            persistent index;%define and initialize persistent index of images, otherwise value lost after calling 
             if isempty(index)
-                index=ir.start;
+                index=ir.start;%start from ir.start 
             end
             left = zeros(600,800,3*(ir.N+1));
             right = left;%initialize left and right for speeding up
-            if index+ir.N<numel(ir.allimage_left)%if not reach the end
-                for i=1:ir.N+1
+            if index+ir.N<numel(ir.allimage_left)%if not reached the end
+                for i=1:ir.N+1%read N+1 images and pack them together to left and right
                     left(:,:,3*i-2:3*i)=im2double(imread(strcat(ir.l,ir.allimage_left(index).name)));
                     right(:,:,3*i-2:3*i)=im2double(imread(strcat(ir.r,ir.allimage_right(index).name)));
-                    index=index+1;%update index
+                    index=index+1;%update index for next image
                 end
-            else%reach the end, set loop, and set start to begin
-                num_rest=numel(ir.allimage_left)-index+1;
-                left = zeros(600,800,3*num_rest);%redefine dimension
+            else%reached the end, set loop, and set start to begin
+                num_rest=numel(ir.allimage_left)-index+1;%number of the rest images
+                left = zeros(600,800,3*num_rest);%redefine dimension because there are less than N+1 images left
                 right = left;
-                for i=1:num_rest%all rest image together, less than N+1
+                for i=1:num_rest%pack all rest image together and pass it to left and right
                     left(:,:,3*i-2:3*i)=im2double(imread(strcat(ir.l,ir.allimage_left(index).name)));
                     right(:,:,3*i-2:3*i)=im2double(imread(strcat(ir.r,ir.allimage_right(index).name)));
-                    index=index+1;%update index
+                    index=index+1;%update index for next image
                 end
-                ir.loop=1;%set loop
-                index=1;%reset to beginning
+                ir.loop=1;%set loop 1 to escape the while loop in challenge
+                index=1;%reset to beginning for next call of this function
             end
             loop = ir.loop;%pass loop to output
         end
