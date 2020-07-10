1907311a5ca4ac1440eb83ee248f012457c18de3
lzw16500 <67428982+lzw16500@users.noreply.github.com>
2020-07-10 18:31:02 +0800

    Update render.m

--- render.m
+++ render.m
@@ -8,23 +8,22 @@ function [result] = render(frame,mask,bg,mode)
   %              overlay: colored, transparent, overlayed
   %              substitute: set background as virtual bg
 
-  fg_here(:,:,1:3)=frame(:,:,1:3).*mask; %apply mask to get foreground 
+  fg_here(:,:,1:3)=frame(:,:,1:3).*mask;  %apply mask to get foreground 
   bg_here(:,:,1:3)=frame(:,:,1:3).*(abs(mask-1)); %apply mask to get background 
-  bg_virtual(:,:,1:3)=bg(:,:,1:3).*(abs(mask-1));%get virtual background from image
-  result=zeros(size(frame));%predefine result 
-  
-  switch render_mode
+  bg_virtual(:,:,1:3)=bg(:,:,1:3).*(abs(mask-1)); %get virtual background from image
+  result=zeros(size(frame)); %predefine result 
+  switch mode
       case 'foreground'
-          result=fg_here;%the rest is already 0, i.e. black
+          result=fg_here; %the rest is already 0, i.e. black
       case 'background'
-          result=bg_here;%the rest is already 0, i.e. black
+          result=bg_here; %the rest is already 0, i.e. black
       case 'overlay'
-          ph_overlay=zeros(size(frame));%both will be showed, transparently colored
-          [ind_0]=find(mask==0);
-          [ind_1]=find(mask==1);
-          rgb1=[0 0.7 0.9];  % the background is blue
-          rgb2=[1 1 0];  % the foreground is yellow
-          for i=1:3
+          ph_overlay=zeros(size(frame)); %both background and foreground will be showed, transparently colored
+          [ind_0]=find(mask==0); % find the background pixels
+          [ind_1]=find(mask==1); % find the foreground pixels
+          rgb1=[0 0.7 0.9];  % the background is set to blue
+          rgb2=[1 1 0];  % the foreground is set to yellow
+          for i=1:3  % convert the gray image into transparently colored image 
               col=ph_overlay(:,:,i);
               col(ind_0)=frame(ind_0)*rgb1(i);
               col(ind_1)=frame(ind_1)*rgb2(i);
@@ -32,8 +31,8 @@ function [result] = render(frame,mask,bg,mode)
           end
           result=ph_overlay;
       case 'substitute'
-          result=fg_here+bg_virtual;%apply virtual background 
+          result=fg_here+bg_virtual; %apply virtual background 
       otherwise
-          warning('Unexpected mode. Black image exported.')%error message
+          warning('Unexpected mode. Black image exported.') %error message
   end
 end
